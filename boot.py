559022eef5bf41bb4fc34c3ccb54a33324109892
FabioNagamine <61300588+FabioNagamine@users.noreply.github.com>
2020-08-04 08:39:48 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,15 @@
+import gc
 import network
 
-def connect()
-    
+
+ap_if = network.WLAN(network.AP_IF)
+ap_if.active(False)
+
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+    sta_if.active(True)
+    sta_if.connect('teste', 'realtime')
+    while not sta_if.isconnected():
+        pass
+
+gc.collect()
